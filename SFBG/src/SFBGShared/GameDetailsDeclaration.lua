6be2ad2bb54b80efb1967f84ed0fe220b6d3d4fe
Doug Banks <doug.banks@gmail.com>
2024-07-12 12:13:57 -0700

    latest, may or may not work

--- SFBG/src/SFBGShared/GameDetailsDeclaration.lua
+++ SFBG/src/SFBGShared/GameDetailsDeclaration.lua
@@ -1,18 +1,32 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RobloxBoardGameShared = ReplicatedStorage.RobloxBoardGameShared
-local CommonTypes = require(RobloxBoardGameShared.Types.CommonTypes)  
+local CommonTypes = require(RobloxBoardGameShared.Types.CommonTypes)
 
 local GameDetailsDeclaration = {}
 
 GameDetailsDeclaration.nutsGameId = 1
 
+local nutsGameOptions = {
+    {
+        name = "\"Schmoozing\" Expansion",
+        optionId = 1,
+        description = "Players attend may gain powerful advantages by bribing the right people.",
+    },
+    {
+        name = "\"Aggressive Squirrel\" Variant",
+        optionId = 2,
+        description = "The Squirrel is more likely to Hunt and Scamper.",
+    },
+} :: CommonTypes.GameOptions
+
 local nutsGameDetails: CommonTypes.GameDetails = {
-    gameId = GameDetailsDeclaration.nutsGameId, 
+    gameId = GameDetailsDeclaration.nutsGameId,
     gameImage = 17571483456,
-    name = "Nuts", 
+    name = "Nuts",
     description = "Ship nuts, and watch out for that squirrel!.",
     minPlayers = 2,
     maxPlayers = 5,
+    gameOptions = nutsGameOptions,
 }
 
 local gameDetailsByGameId: CommonTypes.GameDetailsByGameId = {
